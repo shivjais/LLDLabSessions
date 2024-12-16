cb963b57149405a2e63b87ce3cc795778108df83
Shivam Jaiswal <shivamj1008@gmail.com>
2024-12-16 22:44:16 +0530

    Adder Subtractor Sync block

---
+++ Backend_LLD_1/src/Synchronization/AdderSubtractorSync/Subtractor.java
@@ -0,0 +1,26 @@
+package Synchronization.AdderSubtractorSync;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.locks.Lock;
+
+public class Subtractor implements Callable<Void> {
+    private Value val;
+
+    public Subtractor(Value val){
+        this.val = val;
+
+    }
+    @Override
+    public Void call() throws Exception {
+        for(int i=1;i<=10000;i++){
+            //synchronized will take implicit lock on the object
+            // we must need to pass shared object in sync block
+            synchronized (val){
+                val.setVal(val.getVal()-i);
+            }
+            //release lock after critical section
+
+        }
+        return null;
+    }
+}
