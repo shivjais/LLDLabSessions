2119467233a1871551c63cbf4f68a844f227b8d3
Shivam Jaiswal <shivamj1008@gmail.com>
2024-12-20 09:05:16 +0530

    Update Client.java

--- Backend_LLD_1/src/oopsIntro/Client.java
+++ Backend_LLD_1/src/oopsIntro/Client.java
@@ -2,12 +2,7 @@ package oopsIntro;
 
 public class Client {
     public static void main(String[] args) {
-        C obj = new C();
-        System.out.println(obj.d1);
-        System.out.println(obj.d);
-        obj.fun1();
-        obj.fun();
-        obj.sfun();
+
 
     }
 }
